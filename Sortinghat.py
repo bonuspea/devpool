123722c2010c9ac4e82e2550d2af0987e027d783
Bonus Songwijit <bonuspeaa@gmail.com>
2022-04-27 15:34:52 +0700

    first commit

---
+++ Sortinghat.py
@@ -0,0 +1,28 @@
+import random
+import pandas as pd
+
+student = pd.read_excel('/Users/bonus/workspace/devpool/peple.xlsx',sheet_name='Sheet1') 
+students = student['name'].tolist()
+
+def make_random_groups(students, number_of_groups):
+    
+    #Shuffle list of students
+    random.shuffle(students)
+    
+    #Create groups
+    all_groups = []
+    for index in range(number_of_groups):
+        group = students[index::number_of_groups]
+        all_groups.append(group)
+    
+    #Format and display groups
+    home = ['Gryffindor','Ravenclaw','Hufflepuff','Slytherin']
+    
+    for index, group in enumerate(all_groups):
+        print(f"{home[index]}: {', '.join(group)}")
+        print(f"number of people :{len(group)}")
+        print('----------------')
+    
+    
+
+make_random_groups(students, 4)
